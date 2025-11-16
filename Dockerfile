ec28504bcc6a8d15e4be8e10c97ac05b446f4ba2
snow___shower <wonsb25@gmail.com>
2025-11-16 18:10:59 +0900

    ci: Docker, GitHub Actions 기반 배포 자동화

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,12 @@ FROM python:3.13.6
 
 WORKDIR /app
 
-COPY 
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+RUN playwright install
+
+COPY . .
+
+CMD ["python", "nangnoon.py"]
--- cogs/checkin.py
+++ cogs/checkin.py
@@ -76,4 +76,4 @@ class Checkin(commands.Cog):
             await interaction.followup.send("❌ 출석체크 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.", ephemeral=True)
 
 async def setup(bot):
-    await bot.add_cog(heckin(bot))
+    await bot.add_cog(Checkin(bot))
